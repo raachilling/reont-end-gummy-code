6504daeac51be3813455a05928e0127cac4add1e
raachilling <raquelenciso1@hotmail.com>
2021-04-11 19:54:41 +0100

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -7,15 +7,41 @@ export function fetchUserPurchases() {
         type: SET_USER_PURCHASES,
         payload: [
             {
-                _id: 0,
-                title: 'purchase 1',
-                amount: 8.02
+                _id: 1,
+                total: 19.40, 
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens', 
+                    shippingAddress: '1234 West State Street'
+                }
+
             },
             {
-                _id: 1,
-                title: 'purchase 2',
-                amount: 19.40
-            }
+                _id: 2,
+                total: 1.30, 
+                orderNumber: '82YNDOQ024',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Julliette Harrison', 
+                    shippingAddress: '44 Lake Lane Street'
+                }
+
+            },
+            {
+                _id: 3,
+                total: 11.45, 
+                orderNumber: '173BSO23',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Laura Fernandez', 
+                    shippingAddress: '728 Berkeley Avenue'
+                }
+
+            }, 
         ]
     })
 }
