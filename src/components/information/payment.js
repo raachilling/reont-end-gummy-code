c197b19647c685cea54a5cca97edb5d975b29007
raachilling <raquelenciso1@hotmail.com>
2021-04-29 21:12:07 +0100

    structured shipping js and shipping form

--- src/components/information/payment.js
+++ src/components/information/payment.js
@@ -7,6 +7,8 @@ import PageTitle from '../pageTitle';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
+import PaymentForm from './paymentForm';
+
 class Payment extends Component {
 
     componentDidMount() {
